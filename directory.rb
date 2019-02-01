99816f7e5542e4733fd185e2a7a3ef11505489e0
Alastair Revell <ar15153@outlook.com>
2019-02-01 13:43:31 +0000

    save students

--- directory.rb
+++ directory.rb
@@ -1,33 +1,140 @@
 def print_header
-  puts "The students of Villains Academy"
-  puts "—————————————————————————————————"
-end
-
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+  puts "The students of Villains Academy".center(100)
+  puts "—————————————————————————————————".center(100)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{@students.count} great student(s)".center(100)
+end
+
+def print(students)
+    index = 0
+    if !@students.empty?
+    while index < @students.length do
+    #students.each_with_index do |student, index|
+      puts "#{index + 1}.  #{@students[index][:name]}, #{@students[index][:country]} is joining (#{@students[index][:cohort]}) they are #{@students[index][:height]}cm tall".center(100)
+      index = index + 1
+    end
+    else
+      puts "No students to print".center(100)
+    end
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
+  @students = []
+  puts "Please enter the names of the students".center(100)
+  puts "To finish, just hit return twice".center(100)
   name = gets.chomp
 
+  cohort = add_cohort
+  puts "Please enter the country where #{name} was born: ".center(100)
+  country = gets.chomp
+  puts "Please enter #{name}'s height in cm".center(100)
+  height = gets.chomp
+
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
+      @students << {name: name, cohort: cohort.to_sym, country: country, height: height}
+      puts "Now we have #{@students.count} student(s). Do you want to enter another student's name? ".center(100)
+      puts "If not just hit return".center(100)
+      name = gets.chomp
+      if name.empty?
+        break
+      end
+      cohort = add_cohort
+      puts "Please enter the country where #{name} was born: ".center(100)
+      country = gets.chomp
+      puts "Please enter #{name}'s height in cm".center(100)
+      height = gets.chomp
+    end
+  @students
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def add_cohort
+  cohort_hash = { 1 => :January, 2 => :Feburary, 3 => :March, 4 => :April,
+    5 => :May, 6 => :June, 7 => :Jully, 8 => :August, 9 => :September,
+    10 => :October, 11 => :November,
+    12 => :December}
+    puts "Which month's cohort would you like to join (1-12): ".center(100)
+    num = gets.strip.to_i
+    if cohort_hash.has_key?num
+      cohort = cohort_hash[num]
+    else
+      cohort = cohort_hash[1]
+    end
+    cohort
+end
+
+def sort_by_month
+  puts "Please enter the month you want to sort by: "
+  month = gets.chomp.capitalize.to_sym
+  @students = @students.map {|h| h[:cohort] == month ? h : nil}.compact
+  return @students
+end
+
+def run_programme
+  input_students
+  sort_by_month
+  print_header
+  students = @students
+  print(students)
+  print_footer(students)
+end
+
+def enter_students
+  if @students != nil
+    @students = input_students
+  else
+    puts "No students to print".center(100)
+  end
+end
+
+def show_students
+  print_header
+  students = @students
+  print(students)
+  print_footer(students)
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def process(selection)
+  case selection
+  when "1"
+    enter_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save students to file"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
---
+++ test.rb
@@ -0,0 +1,1 @@
+s = gets.center(100).chomp
